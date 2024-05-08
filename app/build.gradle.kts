120bed959c9a69b108169717b13bd7c0e61128d4
Görkem <emircan9921@outlook.com>
2024-05-08 21:59:26 +0300

    Activated Retrofit library and its permissions.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,10 +33,13 @@ android {
 }
 
 dependencies {
+    implementation ("com.squareup.retrofit2:retrofit:2.7.2")    // Added Retrofit libraries for Internet connections
+    implementation ("com.squareup.retrofit2:converter-gson:2.7.2")
+    implementation ("com.squareup.okhttp3:okhttp:3.14.7")
 
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
