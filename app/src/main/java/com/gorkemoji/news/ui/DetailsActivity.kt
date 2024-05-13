b878a298320e99108553c16fad0aa09f2191ef61
Görkem <emircan9921@outlook.com>
2024-05-13 18:45:47 +0300

    Added going to source feature.

--- app/src/main/java/com/gorkemoji/news/ui/DetailsActivity.kt
+++ app/src/main/java/com/gorkemoji/news/ui/DetailsActivity.kt
@@ -1,5 +1,6 @@
 package com.gorkemoji.news.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bumptech.glide.Glide
@@ -25,5 +26,19 @@ class DetailsActivity : AppCompatActivity() {
         binding.date.text = article.date
         binding.content.text = article.content
 
+        binding.sourceBtn.setOnClickListener {
+            val intent = Intent(this, WebActivity::class.java)
+            intent.putExtra("article", article)
+            startActivity(intent)
+        }
+
+        binding.backBtn.setOnClickListener { onBackPressed() }
+
+    }
+    @Deprecated("Deprecated in Java",
+        ReplaceWith("super.onBackPressed()", "androidx.appcompat.app.AppCompatActivity")
+    )
+    override fun onBackPressed() {
+        super.onBackPressed()
     }
 }
--- app/src/main/java/com/gorkemoji/news/ui/WebActivity.kt
+++ app/src/main/java/com/gorkemoji/news/ui/WebActivity.kt
@@ -1,24 +1,43 @@
 package com.gorkemoji.news.ui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.webkit.WebViewClient
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.gorkemoji.news.R
+import com.gorkemoji.news.data.Article
 import com.gorkemoji.news.databinding.ActivityDetailsBinding
 import com.gorkemoji.news.databinding.ActivityWebBinding
 
 class WebActivity : AppCompatActivity() {
     private lateinit var binding: ActivityWebBinding
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityWebBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val article = intent.getSerializableExtra("article") as Article
 
+        binding.webView.webViewClient = WebViewClient()
+        binding.webView.loadUrl(article.url)
+        binding.webView.settings.javaScriptEnabled = true
+        binding.webView.settings.setSupportZoom(true)
 
+        binding.backBtn.setOnClickListener {
+            onBackPressed()
+        }
 
+    }
+
+    @Deprecated("Deprecated in Java",
+        ReplaceWith("super.onBackPressed()", "androidx.appcompat.app.AppCompatActivity")
+    )
+    override fun onBackPressed() {
+        super.onBackPressed()
     }
 }
